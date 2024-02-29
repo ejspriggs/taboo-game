caac4b688f266d4f3e32ea4ad32ebe90d6f06cd8
Edward Spriggs <ejspriggs@gmail.com>
2024-02-29 12:59:24 -0500

    For some reason the default cursor isn't a pointing hand when a form submit button is outside its form, so override that on the Card Details page.

--- frontend/src/components/CardCreateEdit/index.jsx
+++ frontend/src/components/CardCreateEdit/index.jsx
@@ -113,7 +113,7 @@ function CardCreateEdit({ edit, loginStatus }) {
                 <input
                     type="submit"
                     form="myform"
-                    className="text-white bg-blue-500 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5"
+                    className="text-white cursor-pointer bg-blue-500 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5"
                     value="Save"
                 />
             </div>
