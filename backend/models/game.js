64b1f5f2e0c1f99bc80f1e251719562d981a443d
Edward Spriggs <ejspriggs@gmail.com>
2024-02-29 16:20:09 -0500

    Better formatting for game model.

--- backend/models/game.js
+++ backend/models/game.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const gameSchema = new mongoose.Schema({
-    players: [{ name: String, playerToken: String }],
+    players: [{
+        name: { type: String, required: true },
+        playerToken: { type: String, required: true }
+    }],
     cardholder: { type: String, required: true },
     currentTurn: { type: Number, required: true },
     deck: [{
