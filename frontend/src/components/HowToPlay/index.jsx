04bba3d845c35c27fe865e56575d1b1a5caa278f
Edward Spriggs <ejspriggs@gmail.com>
2024-03-05 11:10:09 -0500

    More tweaks to instructions.

--- frontend/src/components/HowToPlay/index.jsx
+++ frontend/src/components/HowToPlay/index.jsx
@@ -7,10 +7,10 @@ export default function HowToPlay() {
                     Sign up/in from the nav bar above.
                 </li>
                 <li>
-                    Authenticated users can edit a shared deck of cards, create a game with the cards that currently exist,
-                    take over games they&apos;ve created (in case they clear site data, and lose their superuser status),
-                    or delete games they&apos;ve created.  They can also generate join/play links for games.  The superuser/owner
-                    of a game (the original player created when the game is created) can kick out other users.
+                    Authenticated users can edit a shared deck of cards, create a game by shuffling the cards that currently exist,
+                    take over games they&apos;ve created (in case they clear site data, and lose their player token), or delete games
+                    they&apos;ve created.  They can also generate join/play links for games.  The superuser/owner of a game (the
+                    original player created when the game is created) can kick out other users.
                 </li>
                 <li>
                     Anyone can join and participate in a game, with a link from an authenticated user.
@@ -25,6 +25,13 @@ export default function HowToPlay() {
                     only 50% of the players are actually participating at any given time, and the absolute minimum number of players
                     is four. Since Taboo is a fairly high-trust game, I basically never play it with points, and this implementation
                     reflects that house rule.
+                </li>
+                <li>
+                    Sometimes, browsers try to be clever about periodically polling data from servers.  If your client stops updating, just
+                    reload the page.  Your game state is stored exclusively on the server, and your browser stores only a game token (in
+                    the URL) and player token (in the localStorage object).  You won&apos;t lose your place in the game with a reload,
+                    or by navigating away from the page and back. You can play any number of games at once, limited only by the size of the
+                    localStorage instance your browser allocates to each website.
                 </li>
             </ul>
         </div>
